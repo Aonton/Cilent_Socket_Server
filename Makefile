8dfeeb9962b6a4c77655e936a1b10931f533aedc
Amy Feng <afeng5@fordham.edu>
2018-04-07 16:39:36 -0400

    Added comments to executor and makefile. There are detailed instructions on how to use them

--- Makefile
+++ Makefile
@@ -1,4 +1,18 @@
 # Make File for Server & Client Project
+# Amy Feng
+# April 7, 2018
+# How to use this make file
+# OPTION:
+# 1) type "make all" on the command line
+# Purpose: This will build/compile the server.c
+# and client.c files. Then to run the programs,
+# run their executables.
+# ./server and ./client
+# *** Note: You must start the server before the client ***
+# 2) type "make clean"
+# Purpose: removes object files. Sometimes it is needed to
+# remove the object files so taht you code runs with the
+# newest code changes. 
 
 CC = gcc
 CFLAGS = -c
--- executor.c
+++ executor.c
@@ -3,6 +3,26 @@
 #include <string.h>
 #include <unistd.h>
 
+/* Amy Feng
+  April 7, 2018
+  Note: For the person adapting this
+        function into the client/server
+        code. My advice is either copy this
+        function into the server file or
+        use a file pointer to reference this
+        function. I suggest the latter for
+        code clarity. The client code should
+        send the command to the server and
+        the server should take that command
+        and pass it to this function (see below
+        for how to pass arguments into this function).
+        The server should take the file pointer returned
+        from this function (see below for info on the return
+        variable). The server should send the output line by
+        line to the client (see below in the ouput section for
+        suggestion on how to do this)
+*/
+
 /* INPUT: input a string/char* that is
    the command that the user wants to execute
    on the server. The command should be put in
@@ -22,9 +42,19 @@
    }
 */
 
+/* This function executes a command
+   and return the command's ouput.
+*/
 FILE* executor(char* command)
 {
-  FILE* cmd_output;
+  // cmd_output has the command output
+  FILE* cmd_output = NULL;
+
+  // command should not be NULL
+  // server code should check to make
+  // sure the file pointer (return value)
+  // is not null. If it is, please handle
+  // it how you see fit.
   if(command!=NULL)
   {
     int result;
@@ -32,7 +62,10 @@ FILE* executor(char* command)
     char buffer[1024];
     int status;
 
+    // creating pipes
     result = pipe(pipefd);
+
+    // checks for pipe creation error
     if(result<0)
     {
       printf("Pipe Error");
@@ -41,35 +74,52 @@ FILE* executor(char* command)
 
     //start forking
     result = fork();
+
+    // checks for fork creation error
     if(result<0)
     {
       printf("Fork Failed\n");
       exit(0);
     }
+    // child process
     else if(result==0)
     {
+      // directing STDOUT and STDERR consoles
+      // to pipes (pipefd)
       dup2(pipefd[1], STDOUT_FILENO);
       dup2(pipefd[1], STDERR_FILENO);
       close(pipefd[0]);
       close(pipefd[1]);
+
+      // executes the command
+      // since the output and error buffers
+      // have been redirected, the output will
+      // not go to the console and instead go into
+      // the pipes
       execl("/bin/sh", "/bin/sh", "-c", command, 0);
       exit(0);
     }
+    // parent process
     else
     {
       close(pipefd[1]);
 
+      // dumping data from pipes into
+      // file pointer (cmd_output)
       cmd_output = fdopen(pipefd[0], "r");
 
-      // cmd_output has the command output
+      // This just displays the output/error.
+      // This can be remove later or kept.
       while (fgets(buffer, sizeof buffer, cmd_output) != NULL)
       {
         printf("Data from command: %s\n", buffer);
       }
 
+      // parent processes must wait for the child process
       wait(&status);
     }
   }
+  // Error message for null command
   else
   {
     printf("No command received correctly\n");
