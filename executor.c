75c97a05b977cdd1eece8727e15afbb2bcccfa78
Saul A. Soto <developingdevelopers@outlook.com>
2018-04-15 22:12:29 -0400

    origin executor.c

--- executor.c
+++ executor.c
@@ -2,9 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-//#include "executor.h"
 
 /* Amy Feng
   April 7, 2018
@@ -48,12 +45,10 @@
 /* This function executes a command
    and return the command's ouput.
 */
-
-char* executor(char* command)
+FILE* executor(char* command)
 {
   // cmd_output has the command output
-  FILE* cmd_output;// = NULL;
-  char* buffer;
+  FILE* cmd_output = NULL;
 
   // command should not be NULL
   // server code should check to make
@@ -64,6 +59,7 @@ char* executor(char* command)
   {
     int result;
     int pipefd[2];
+    char buffer[1024];
     int status;
 
     // creating pipes
@@ -111,45 +107,12 @@ char* executor(char* command)
       // dumping data from pipes into
       // file pointer (cmd_output)
       cmd_output = fdopen(pipefd[0], "r");
-      /*DOES NOT WORK
-      	fseek(cmd_output, 0L, SEEK_END);
-      	lSize = ftell(cmd_output);
-      	fseek(cmd_output,0L,SEEK_SET);
-      	rewind(cmd_output);
-      	printf("%lu",lSize);
-      */
 
-      /* allocate memory for entire content */
-      buffer = malloc(1024);
-      
       // This just displays the output/error.
       // This can be remove later or kept.
-      printf("Data from command: ");
-      /* MIGHT HAVE SOMETHING HERE
-      for(int i=0;i<sizeof(cmd_output);i++) {
-      	buffer[i] = cmd_output->_IO_read_ptr[i];
-      }*/
-      int j=0;
-      char temp[300];
-      /* COMMAND DATA IS RETRIEVED IN LAYERS (i.e. first line, then second line, third, etc.)
-       * TRYING TO APPEND NEW LINE OF COMMAND DATA TO BUFFER SO THAT ALL COMMAND DATA
-       * CAN BE SENT AT ONCE TO CLIENT
-      */
-      while(fgets(temp, sizeof(temp), cmd_output) != NULL)
+      while (fgets(buffer, sizeof buffer, cmd_output) != NULL)
       {
-	for(int i=0;i<sizeof(temp);i++) {
-		if((j<=sizeof(buffer)-1) && (temp[i] != '\0')) {
-			buffer[j] = temp[i];//cmd_output->_IO_read_ptr[i];
-			j++;
-		}
-		else
-			break;
-	}
-	//memset(temp, 0, sizeof(temp));
-	//THE LINE BELOW PRINTS ONLY THE FIRST LINE OF COMMAND DATA
-	printf("%s\n",buffer);
-	//IF YOU USE THE LINE BELOW ALL DATA FROM COMMAND IS PRINTED CORRECTLY
-	//printf("%s\n",temp);
+        printf("Data from command: %s\n", buffer);
       }
 
       // parent processes must wait for the child process
@@ -159,9 +122,8 @@ char* executor(char* command)
   // Error message for null command
   else
   {
-    //printf("No command received correctly\n");
-     strcpy(buffer,"The client's command does not exist");
+    printf("No command received correctly\n");
   }
 
-  return buffer;
+  return cmd_output;
 }
